c322d5eefc5e42d54db17d10429b58525827a90c
Sadewma Marasinghe <sadewmamarasinghe@gmail.com>
2026-07-19 15:04:51 +0530

    User Class Created

--- RecruitmentPlatform.API/Entities/User.cs
+++ RecruitmentPlatform.API/Entities/User.cs
@@ -1,17 +1,21 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace RecruitmentPlatform.API.Entities
+namespace RecruitmentPlatform.API.Entities;
+
+public class User
 {
-    public class User
-    {
-        [Key]
-        public int UserId { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
-        public string Role { get; set; } = "Candidate"; // Admin, Recruiter, HiringManager, Candidate
+    [Key]
+    public int UserId { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string Role { get; set; } = "Candidate";
 
-        public ICollection<JobPosting> JobPostings { get; set; } = new List<JobPosting>();
-    }
+    [ForeignKey("Organization")]
+    public int? OrganizationId { get; set; }
+    public Organization? Organization { get; set; }
+
+    public ICollection<JobPosting> JobPostings { get; set; } = new List<JobPosting>();
 }
