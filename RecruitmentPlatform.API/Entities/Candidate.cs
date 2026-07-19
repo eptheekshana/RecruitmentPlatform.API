eb3e2eb564d7e366b423fc8a46a9120806f7b9b3
eptheekshana <eptheekshana@gmail.com>
2026-07-19 15:15:10 +0530

    Created Candidate.cs

--- RecruitmentPlatform.API/Entities/Candidate.cs
+++ RecruitmentPlatform.API/Entities/Candidate.cs
@@ -1,13 +1,21 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace RecruitmentPlatform.API.Entities
-{
-    public class Candidate : User
-    {
-        public string ResumeUrl { get; set; } = string.Empty;
-        public string Skills { get; set; } = string.Empty; // Extracted skills using AI
+namespace RecruitmentPlatform.API.Entities;
 
-        public ICollection<Application> Applications { get; set; } = new List<Application>();
-    }
+public class Candidate
+{
+    [Key]
+    public int CandidateId { get; set; }
+
+    [ForeignKey("User")]
+    public int UserId { get; set; }
+    public User? User { get; set; }
+
+    public string ResumeUrl { get; set; } = string.Empty;
+    public string Skills { get; set; } = string.Empty;
+    public string Bio { get; set; } = string.Empty;
+    public string ExperienceLevel { get; set; } = string.Empty;
+
+    public ICollection<Application> Applications { get; set; } = new List<Application>();
 }
