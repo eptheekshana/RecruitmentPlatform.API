96d3213fc7e1535430fc6e98eb40068097eb2123
eptheekshana <eptheekshana@gmail.com>
2026-07-19 15:17:59 +0530

    Database

--- RecruitmentPlatform.API/Data/ApplicationDbContext.cs
+++ RecruitmentPlatform.API/Data/ApplicationDbContext.cs
@@ -1,36 +1,66 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using RecruitmentPlatform.API.Entities;
 
-namespace RecruitmentPlatform.API.Data
+namespace RecruitmentPlatform.API.Data;
+
+public class ApplicationDbContext : DbContext
 {
-    public class ApplicationDbContext : DbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
+    }
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Candidate> Candidates { get; set; }
-        public DbSet<JobPosting> JobPostings { get; set; }
-        public DbSet<Application> Applications { get; set; }
-        public DbSet<InterviewSchedule> InterviewSchedules { get; set; }
-        public DbSet<Organization> Organizations { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Candidate> Candidates { get; set; }
+    public DbSet<JobPosting> JobPostings { get; set; }
+    public DbSet<Application> Applications { get; set; }
+    public DbSet<InterviewSchedule> InterviewSchedules { get; set; }
+    public DbSet<Organization> Organizations { get; set; }
+    public DbSet<AuditLog> AuditLogs { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Application>()
-                .HasOne(a => a.Candidate)
-                .WithMany(c => c.Applications)
-                .HasForeignKey(a => a.CandidateId)
-                .OnDelete(DeleteBehavior.NoAction);
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.Organization)
+            .WithMany(o => o.Employees)
+            .HasForeignKey(u => u.OrganizationId)
+            .OnDelete(DeleteBehavior.SetNull);
 
-            modelBuilder.Entity<JobPosting>()
-                .HasOne(j => j.Recruiter)
-                .WithMany(u => u.JobPostings)
-                .HasForeignKey(j => j.RecruiterId)
-                .OnDelete(DeleteBehavior.NoAction);
-        }
+        modelBuilder.Entity<Candidate>()
+            .HasOne(c => c.User)
+            .WithMany()
+            .HasForeignKey(c => c.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Application>()
+            .HasOne(a => a.Candidate)
+            .WithMany(c => c.Applications)
+            .HasForeignKey(a => a.CandidateId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Application>()
+            .HasOne(a => a.JobPosting)
+            .WithMany()
+            .HasForeignKey(a => a.JobId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<JobPosting>()
+            .HasOne(j => j.Recruiter)
+            .WithMany(u => u.JobPostings)
+            .HasForeignKey(j => j.RecruiterId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<InterviewSchedule>()
+            .HasOne(i => i.Application)
+            .WithMany()
+            .HasForeignKey(i => i.ApplicationId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<InterviewSchedule>()
+            .HasOne(i => i.Interviewer)
+            .WithMany()
+            .HasForeignKey(i => i.InterviewerId)
+            .OnDelete(DeleteBehavior.SetNull);
     }
 }
